c66297a46843cfdced67243cb9c0adbef0b1672b
Rakib Hosain <25496054+rakibctg@users.noreply.github.com>
2023-06-09 19:32:57 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by RAKIB Hosain</h3>
+          <h3>Designed and Developed by RAKIB HOSAIN</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} RH</h3>
