594f470df50faf578aadf23d89d28e385cdf8e48
MOHAMMAD EUNUS <25496054+rakibctg@users.noreply.github.com>
2024-11-24 01:58:08 +0800

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -77,7 +77,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://twitter.com/rakibctg786"
+                  href="https://twitter.com/eunusctg"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -97,7 +97,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/rakibctg786"
+                  href="https://www.instagram.com/eunusctg"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
